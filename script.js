bb20a98d876352510c50d85f10998bce7afe7c27
evertonlion-dev <evertonsantosleao@outlook.com>
2026-04-10 10:51:26 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -9,43 +9,50 @@ function addExpense() {
 }
 
 function addTransaction(type) {
-  const desc = document.getElementById("desc").value = "";
-  const value = Number(document.getElementById("value").value = "");
+  const desc = document.getElementById("desc").value;
+  const value = Number(document.getElementById("value").value);
 
   if (!desc || !value) return;
 
   transactions.push({ desc, value, type });
 
+  document.getElementById("desc").value = "";
+  document.getElementById("value").value = "";
+
   saveData();
   update();
-  if (transactions.length === 0) {
-  list.innerHTML = "<p>Nenhuma transação ainda</p>";
-  balanceEl.textContent = 0;
-  return;
-}
 }
 
 function update() {
   const list = document.getElementById("list");
   const balanceEl = document.getElementById("balance");
-  
 
   list.innerHTML = "";
+
+  if (transactions.length === 0) {
+    list.innerHTML = "<p>Nenhuma transação ainda</p>";
+    balanceEl.textContent = "R$ 0,00";
+    return;
+  }
+
   let balance = 0;
 
   transactions.forEach((t, index) => {
     const li = document.createElement("li");
 
     if (t.type === "entrada") {
-      li.textContent = `${t.desc}: +R$${t.value}`;
+      li.textContent = `${t.desc}: +${t.value}`;
       balance += t.value;
+      li.style.borderLeft = "5px solid #00e676";
     } else {
-      li.textContent = `${t.desc}: -R$${t.value}`;
+      li.textContent = `${t.desc}: -${t.value}`;
       balance -= t.value;
+      li.style.borderLeft = "5px solid #ff5252";
     }
 
     const btn = document.createElement("button");
     btn.textContent = "❌";
+
     btn.onclick = () => {
       transactions.splice(index, 1);
       saveData();
@@ -53,24 +60,21 @@ function update() {
     };
 
     li.appendChild(btn);
-    if (t.type === "entrada") {
-  li.style.borderLeft = "5px solid #00e676";
-} else {
-  li.style.borderLeft = "5px solid #ff5252";
-}
     list.appendChild(li);
   });
 
+  // 💰 FORMATAÇÃO EM REAL
   balanceEl.textContent = balance.toLocaleString("pt-BR", {
-  style: "currency",
-  currency: "BRL"
-});
+    style: "currency",
+    currency: "BRL"
+  });
 
+  // 🎨 COR DINÂMICA
   if (balance >= 0) {
-  balanceEl.style.color = "#00e676";
-} else {
-  balanceEl.style.color = "#ff5252";
-}
+    balanceEl.style.color = "#00e676";
+  } else {
+    balanceEl.style.color = "#ff5252";
+  }
 }
 
 function saveData() {
